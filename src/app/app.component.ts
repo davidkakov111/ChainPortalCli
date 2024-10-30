6b5ee780f3d184aef9fe63c682150b675e391e44
davidkov111 <kovacsdavid648@outlook.com>
2024-10-31 00:31:17 +0200

    responsive header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { SharedModule } from './modules/shared/shared.module';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
     RouterOutlet,
-    SharedModule
+    RouterModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  currentYear: number;
+
+  constructor() {
+    this.currentYear = new Date().getFullYear();
+  }
 }
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,23 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FooterComponent } from './components/footer/footer.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    FooterComponent,
-    NavbarComponent
   ],
   imports: [
-    CommonModule,
-    MatToolbarModule,
-    MatButtonModule
   ],
   exports: [
-    FooterComponent,
-    NavbarComponent
   ]
 })
 export class SharedModule { }
