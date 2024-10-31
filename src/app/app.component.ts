721698d13e8a4c8401ae219e9b5fc08455c3eadf
davidkov111 <kovacsdavid648@outlook.com>
2024-10-31 23:26:13 +0200

    updated header positioning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    CommonModule,
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
@@ -17,12 +20,48 @@ import { MatIconModule } from '@angular/material/icon';
     RouterModule
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  // Angular animation for the header
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({transform: 'translateY(0)'})),
+      state('out', style({transform: 'translateY(-100%)'})),
+      transition('in <=> out', [animate('300ms ease-in-out')])
+    ])
+  ]
 })
 export class AppComponent {
   currentYear: number;
+  isNavbar: boolean = true;
+  navbarAnimationIn: boolean = true;
+  private previousScrollPosition = 0;
+  navbarHeight: string = '0px';
+  
+  @ViewChild('navbarHeightEl', { static: false }) navbarHeightEl!: ElementRef;
 
-  constructor() {
-    this.currentYear = new Date().getFullYear();
+  // Get the current year for the footer
+  constructor() {this.currentYear = new Date().getFullYear()};
+
+  // Access the height of the element after the view initializes 
+  ngAfterViewInit() {
+    if (!this.navbarHeightEl.nativeElement.offsetHeight) return;
+    this.navbarHeight = `${this.navbarHeightEl.nativeElement.offsetHeight}px`;
+  }
+
+  // HostListener listens for scroll events on the window
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    // Check if user is scrolling up
+    if (currentScrollPosition < this.previousScrollPosition || currentScrollPosition <= window.innerHeight) {
+      this.isNavbar = true;
+      setTimeout(() => {this.navbarAnimationIn = true;}, 300);
+    } else {
+      this.navbarAnimationIn = false;
+      setTimeout(() => {this.isNavbar = false}, 300);
+    }
+
+    this.previousScrollPosition = currentScrollPosition;
   }
 }
