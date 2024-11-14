2487f55e6b438a78517f4c0de73a5146f550873a
David Kovacs <143946579+davidkakov111@users.noreply.github.com>
2024-11-14 09:02:13 +0200

    production deploy resolved

--- src/app/modules/shared/services/reown-wallet-connect.service.ts
+++ src/app/modules/shared/services/reown-wallet-connect.service.ts
@@ -1,41 +1,46 @@
 import { Injectable } from '@angular/core';
 import { AppKit, createAppKit } from '@reown/appkit/react';
-import { SolanaAdapter } from '@reown/appkit-adapter-solana/react';
 import { solana, solanaTestnet, solanaDevnet } from '@reown/appkit/networks';
-import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
-import { SolflareWalletAdapter } from '@solana/wallet-adapter-solflare';
 import { environment } from '../../../../environments/environment';
 
 @Injectable({ 
   providedIn: 'root'
 })
 export class ReownWalletConnectService {
-  // 0. Set up Solana Adapter
-  solanaWeb3JsAdapter = new SolanaAdapter({
-    wallets: [new PhantomWalletAdapter(), new SolflareWalletAdapter()]
-  })
-
-  // 1. Get projectId from https://cloud.reown.com
-  projectId = environment.reownProjectId;
-
-  // 2. Create a metadata object - optional
-  metadata = {
-    name: 'ChainPortal',
-    description: 'Your all-in-one platform for managing crypto assets!',
-    url: 'https://chainportal.vercel.app/', // origin must match your domain & subdomain
-    icons: ['https://avatars.githubusercontent.com/u/179229932'] // TODO - Maybe i need to change it
-  }
-
   // AppKit Modal
   appKit!: AppKit;
 
-  constructor() {
+  constructor() {this.createModal()}
+
+  // Create modal to connect wallet
+  async createModal() {
+    // Lazy Loading the solana adapters to avoid the build process from attempting to parse the Solana-related code when building.
+    const { SolanaAdapter } = await import('@reown/appkit-adapter-solana/react');
+    const { PhantomWalletAdapter } = await import('@solana/wallet-adapter-phantom');
+    const { SolflareWalletAdapter } = await import('@solana/wallet-adapter-solflare');
+
+    // 0. Set up Solana Adapter
+    const solanaWeb3JsAdapter = new SolanaAdapter({
+      wallets: [new PhantomWalletAdapter(), new SolflareWalletAdapter()]
+    })
+
+    // 1. Get projectId from https://cloud.reown.com
+    const projectId = environment.reownProjectId;
+
+    // 2. Create a metadata object - optional
+    const metadata = {
+      name: 'ChainPortal',
+      description: 'Your all-in-one platform for managing crypto assets!',
+      url: 'https://chainportal.vercel.app/', // origin must match your domain & subdomain
+      icons: ['https://avatars.githubusercontent.com/u/179229932'] // TODO - Maybe i need to change it
+    }
+
     // 3. Create modal
     this.appKit = createAppKit({
-      adapters: [this.solanaWeb3JsAdapter],
+      adapters: [solanaWeb3JsAdapter],
       networks: [solana, solanaTestnet, solanaDevnet],
-      metadata: this.metadata,
-      projectId: this.projectId,
+      metadata: metadata,
+      projectId,
       features: {
         analytics: true // Optional - defaults to your Cloud configuration
       }
@@ -48,4 +53,5 @@ export class ReownWalletConnectService {
 // @reown/appkit-adapter-solana 
 // @solana/wallet-adapter-phantom 
 // @solana/wallet-adapter-solflare
+
 // You can try modifying the code where __filename is referenced using fileURLToPath(import.meta.url) instead.
